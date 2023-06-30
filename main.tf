b213760191b705b0dbab2500faad75920f2d3af0
Fox Pandora <terencher@yahoo.com>
2023-06-30 00:22:51 -0400

    terraform

---
+++ main.tf
@@ -0,0 +1,18 @@
+resource "aws_sns_topic" "demo" {
+  name = var.topic_name
+  tags = {
+         name=var.team
+        
+  }
+}
+  
+
+
+resource "aws_sns_topic_subscription" "email-target" {
+  topic_arn = aws_sns_topic.demo.arn
+  protocol  = var.protocol_type
+  endpoint  = var.end_point
+
+}   
+   
+
